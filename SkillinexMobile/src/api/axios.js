da2765cca4d833ad3f295721989712cd7fb19a03
Tushar <tushardewangan7759@gmail.com>
2026-09-06 00:48:40 +0530

    cloudflared tunnel update

--- SkillinexMobile/src/api/axios.js
+++ SkillinexMobile/src/api/axios.js
@@ -4,7 +4,7 @@ import { Alert } from 'react-native';
 
 // PRO-TIP: Replace with your specific Mac IP 
 // Check this via 'ifconfig' on Mac or 'ipconfig' on Windows
-const API_BASE_URL = 'http://192.168.29.56:5050/'; 
+const API_BASE_URL = 'http://skillinex-api.hacksmiths.dev/'; 
 // const API_BASE_URL = 'http://10.163.110.99:5050/'; 
 
 const api = axios.create({
